2a4dbda47f156f9293e309ed04104512414365c6
verekonn <verekonn@yandex-team.ru>
2022-04-12 14:27:31 +0300

    (+) Init commit 10.

--- appo-war/src/test/java/ru/vkn/appowar/MoveForwardTest.java
+++ appo-war/src/test/java/ru/vkn/appowar/MoveForwardTest.java
@@ -24,7 +24,7 @@ class MoveForwardTest {
     }
 
     @Test
-    void moveForward() {
+    void actOnce() {
         fightCommand.act(
                 technicalUnitSource,
                 posititon);

48cf39e7220271941373110e9a19978ac3eaece8
verekonn <verekonn@yandex-team.ru>
2022-04-21 11:27:47 +0300

    (+) Init commit 10.

--- L06-annotations/src/main/java/ru/otus/annotations/usage/TestDemo.java
+++ L06-annotations/src/main/java/ru/otus/annotations/usage/TestDemo.java
@@ -1,10 +1,6 @@
 package ru.otus.annotations.usage;
 
 
-import ru.otus.annotations.AfterTest;
-import ru.otus.annotations.BeforeTest;
-import ru.otus.annotations.Test;
-
 public class TestDemo {
     public static void main(String[] args) {
         Class<?> clazz = TestAll.class;
